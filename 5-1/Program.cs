17ca6bf0811906c3a14f63844c5df58b4d3dc1eb
MarthaLRigby <martha.l.rigby@gmail.com>
2015-06-03 00:39:47 +0100

    Ex 5-1

---
+++ 5-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=1;
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 10);
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
